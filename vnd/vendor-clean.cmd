2720cef3c91bb16ef203e4c41e2916debe61a872
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-05-02 10:27:59 +0300

    int - Rollout build script improvements
    ---
    
    Type: int
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- vnd/vendor-clean.cmd
+++ vnd/vendor-clean.cmd
@@ -3,4 +3,4 @@
 set ROOTDIR=%~dp0..
 
 echo Cleaning up...
-for %%f in (bin, obj, docs, KSBuild, KSAnalyzer, KSTest, nitrocid-28, nitrocid-28-lite, tmp) do forfiles /s /m %%f /p %ROOTDIR%\ /C "cmd /c echo @path && rd /s /q @path"
+for %%f in (bin, obj, docs, KSBuild, KSAnalyzer, KSTest, nitrocid-28, nitrocid-28-lite, tmp) do forfiles /s /m %%f /p "%ROOTDIR%" /C "cmd /c echo @path && rd /s /q @path"
