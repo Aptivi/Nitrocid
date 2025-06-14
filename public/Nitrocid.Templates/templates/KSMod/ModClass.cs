83eae68f37cd843671b6cb8fb5c0da00cb79f09a
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-06-14 14:05:37 +0300

    ref - Removed code repetition for char buffering
    ---
    
    Type: ref
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModClass.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModClass.cs
@@ -11,7 +11,7 @@ namespace KSMod
 
         public string Version => "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(4, 0, 28, 7);
+        public Version MinimumSupportedApiVersion => new(4, 0, 28, 8);
 
         public ModLoadPriority LoadPriority => ModLoadPriority.Optional;
 
