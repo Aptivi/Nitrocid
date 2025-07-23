5e249094dc5fa4be3aeb6f80a2f1585c0f228640
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-07-23 16:30:36 +0300

    add - prt - Added Clochroma Analog (pt. 2)
    ---
    
    We've finally implemented the screensaver. The only trouble is that we
    need to optimize the analog clock widget to respect the background used.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 2/2

--- public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
@@ -16711,8 +16711,18 @@ namespace Nitrocid.ScreensaverPacks.Settings
         #endregion
 
         #region ClochromaAnalog
-        private bool clochromaanalogBright = false;
-        private int clochromaanalogDelay = 1000;
+        private bool clochromaAnalogTrueColor = true;
+        private int clochromaAnalogDelay = 1000;
+        private bool clochromaAnalogShowSecondsHand = true;
+        private int clochromaAnalogMinimumRedColorLevel = 0;
+        private int clochromaAnalogMinimumGreenColorLevel = 0;
+        private int clochromaAnalogMinimumBlueColorLevel = 0;
+        private int clochromaAnalogMinimumColorLevel = 0;
+        private int clochromaAnalogMaximumRedColorLevel = 255;
+        private int clochromaAnalogMaximumGreenColorLevel = 255;
+        private int clochromaAnalogMaximumBlueColorLevel = 255;
+        private int clochromaAnalogMaximumColorLevel = 255;
+        private bool clochromaAnalogBright = false;
 
         /// <summary>
         /// [ClochromaAnalog] Whether to use bright or dark version.
@@ -16721,11 +16731,25 @@ namespace Nitrocid.ScreensaverPacks.Settings
         {
             get
             {
-                return clochromaanalogBright;
+                return clochromaAnalogBright;
             }
             set
             {
-                clochromaanalogBright = value;
+                clochromaAnalogBright = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] Enable truecolor support. Has a higher priority than 255 color support.
+        /// </summary>
+        public bool ClochromaAnalogTrueColor
+        {
+            get
+            {
+                return clochromaAnalogTrueColor;
+            }
+            set
+            {
+                clochromaAnalogTrueColor = value;
             }
         }
         /// <summary>
@@ -16735,13 +16759,173 @@ namespace Nitrocid.ScreensaverPacks.Settings
         {
             get
             {
-                return clochromaanalogDelay;
+                return clochromaAnalogDelay;
             }
             set
             {
                 if (value <= 0)
                     value = 1000;
-                clochromaanalogDelay = value;
+                clochromaAnalogDelay = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] Shows the seconds hand.
+        /// </summary>
+        public bool ClochromaAnalogShowSecondsHand
+        {
+            get
+            {
+                return clochromaAnalogShowSecondsHand;
+            }
+            set
+            {
+                clochromaAnalogShowSecondsHand = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The minimum red color level (true color)
+        /// </summary>
+        public int ClochromaAnalogMinimumRedColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMinimumRedColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                clochromaAnalogMinimumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The minimum green color level (true color)
+        /// </summary>
+        public int ClochromaAnalogMinimumGreenColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMinimumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                clochromaAnalogMinimumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The minimum blue color level (true color)
+        /// </summary>
+        public int ClochromaAnalogMinimumBlueColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMinimumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                clochromaAnalogMinimumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The minimum color level (255 colors or 16 colors)
+        /// </summary>
+        public int ClochromaAnalogMinimumColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMinimumColorLevel;
+            }
+            set
+            {
+                int FinalMinimumLevel = 255;
+                if (value <= 0)
+                    value = 0;
+                if (value > FinalMinimumLevel)
+                    value = FinalMinimumLevel;
+                clochromaAnalogMinimumColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The maximum red color level (true color)
+        /// </summary>
+        public int ClochromaAnalogMaximumRedColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMaximumRedColorLevel;
+            }
+            set
+            {
+                if (value <= clochromaAnalogMinimumRedColorLevel)
+                    value = clochromaAnalogMinimumRedColorLevel;
+                if (value > 255)
+                    value = 255;
+                clochromaAnalogMaximumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The maximum green color level (true color)
+        /// </summary>
+        public int ClochromaAnalogMaximumGreenColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMaximumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= clochromaAnalogMinimumGreenColorLevel)
+                    value = clochromaAnalogMinimumGreenColorLevel;
+                if (value > 255)
+                    value = 255;
+                clochromaAnalogMaximumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The maximum blue color level (true color)
+        /// </summary>
+        public int ClochromaAnalogMaximumBlueColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMaximumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= clochromaAnalogMinimumBlueColorLevel)
+                    value = clochromaAnalogMinimumBlueColorLevel;
+                if (value > 255)
+                    value = 255;
+                clochromaAnalogMaximumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [ClochromaAnalog] The maximum color level (255 colors or 16 colors)
+        /// </summary>
+        public int ClochromaAnalogMaximumColorLevel
+        {
+            get
+            {
+                return clochromaAnalogMaximumColorLevel;
+            }
+            set
+            {
+                int FinalMaximumLevel = 255;
+                if (value <= clochromaAnalogMinimumColorLevel)
+                    value = clochromaAnalogMinimumColorLevel;
+                if (value > FinalMaximumLevel)
+                    value = FinalMaximumLevel;
+                clochromaAnalogMaximumColorLevel = value;
             }
         }
         #endregion
