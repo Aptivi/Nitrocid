35ea11145bda437b1212af7f399cb7366bcea507
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-09-27 16:12:36 +0300

    ref - Moved name gen commands to Terminaux 8.0 PR
    ---
    
    We've moved name generation commands to Terminaux 8.0 as they are better
    located as unified commands for all shells.
    
    ---
    
    Type: ref
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.Extras.NameGen/NameGenInit.cs
+++ public/Nitrocid.Addons/Nitrocid.Extras.NameGen/NameGenInit.cs
@@ -19,7 +19,6 @@
 
 using Terminaux.Shell.Arguments;
 using Terminaux.Shell.Switches;
-using Nitrocid.Extras.NameGen.Commands;
 using Nitrocid.Extras.NameGen.Localized;
 using Nitrocid.Extras.NameGen.Screensavers;
 using Nitrocid.Extras.NameGen.Settings;
@@ -35,117 +34,6 @@ namespace Nitrocid.Extras.NameGen
 {
     internal class NameGenInit : IAddon
     {
-        private readonly List<CommandInfo> addonCommands =
-        [
-                new CommandInfo("findfirstname", /* Localizable */ "NKS_NAMEGEN_COMMAND_FINDFIRSTNAME_DESC",
-                    [
-                        new CommandArgumentInfo(
-                        [
-                            new CommandArgumentPart(false, "term", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_TERM_DESC"
-                            }),
-                            new CommandArgumentPart(false, "nameprefix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_NAMEPREFIX_DESC"
-                            }),
-                            new CommandArgumentPart(false, "namesuffix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_NAMESUFFIX_DESC"
-                            }),
-                        ],
-                        [
-                            new SwitchInfo("t", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_T_DESC", new SwitchOptions()
-                            {
-                                AcceptsValues = false
-                            }),
-                            new SwitchInfo("male", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_MALE_DESC", new SwitchOptions()
-                            {
-                                ConflictsWith = ["female", "both"],
-                                AcceptsValues = false,
-                            }),
-                            new SwitchInfo("female", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_FEMALE_DESC", new SwitchOptions()
-                            {
-                                ConflictsWith = ["male", "both"],
-                                AcceptsValues = false,
-                            }),
-                            new SwitchInfo("both", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_UNIFIED_DESC", new SwitchOptions()
-                            {
-                                ConflictsWith = ["female", "male"],
-                                AcceptsValues = false,
-                            }),
-                        ], true)
-                    ], new FindFirstNameCommand(), CommandFlags.RedirectionSupported | CommandFlags.Wrappable),
-
-                new CommandInfo("findsurname", /* Localizable */ "NKS_NAMEGEN_COMMAND_FINDSURNAME_DESC",
-                    [
-                        new CommandArgumentInfo(
-                        [
-                            new CommandArgumentPart(false, "term", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_TERM_DESC"
-                            }),
-                            new CommandArgumentPart(false, "surnameprefix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_SURNAMEPREFIX_DESC"
-                            }),
-                            new CommandArgumentPart(false, "surnamesuffix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_SURNAMESUFFIX_DESC"
-                            }),
-                        ], true)
-                    ], new FindSurnameCommand(), CommandFlags.RedirectionSupported | CommandFlags.Wrappable),
-
-                new CommandInfo("genname", /* Localizable */ "NKS_NAMEGEN_COMMAND_GENNAME_DESC",
-                    [
-                        new CommandArgumentInfo(
-                        [
-                            new CommandArgumentPart(true, "namescount", new CommandArgumentPartOptions()
-                            {
-                                IsNumeric = true,
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_GENNAME_ARGUMENT_NAMESCOUNT_DESC"
-                            }),
-                            new CommandArgumentPart(false, "nameprefix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_NAMEPREFIX_DESC"
-                            }),
-                            new CommandArgumentPart(false, "namesuffix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_NAMESUFFIX_DESC"
-                            }),
-                            new CommandArgumentPart(false, "surnameprefix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_SURNAMEPREFIX_DESC"
-                            }),
-                            new CommandArgumentPart(false, "surnamesuffix", new CommandArgumentPartOptions()
-                            {
-                                ArgumentDescription = /* Localizable */ "NKS_NAMEGEN_COMMAND_ARGUMENT_SURNAMESUFFIX_DESC"
-                            }),
-                        ],
-                        [
-                            new SwitchInfo("t", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_T_DESC", new SwitchOptions()
-                            {
-                                AcceptsValues = false
-                            }),
-                            new SwitchInfo("male", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_MALE_DESC", new SwitchOptions()
-                            {
-                                ConflictsWith = ["female", "both"],
-                                AcceptsValues = false,
-                            }),
-                            new SwitchInfo("female", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_FEMALE_DESC", new SwitchOptions()
-                            {
-                                ConflictsWith = ["male", "both"],
-                                AcceptsValues = false,
-                            }),
-                            new SwitchInfo("both", /* Localizable */ "NKS_NAMEGEN_COMMAND_SWITCH_UNIFIED_DESC", new SwitchOptions()
-                            {
-                                ConflictsWith = ["female", "male"],
-                                AcceptsValues = false,
-                            }),
-                        ], true)
-                    ], new GenNameCommand(), CommandFlags.RedirectionSupported | CommandFlags.Wrappable),
-        ];
-
         public string AddonName =>
             InterAddonTranslations.GetAddonName(KnownAddons.ExtrasNameGen);
 
@@ -159,7 +47,6 @@ namespace Nitrocid.Extras.NameGen
         {
             // Initialize everything
             LanguageTools.AddCustomAction(AddonName, new(() => LocalStrings.Languages, () => LocalStrings.Localizations, LocalStrings.Translate, LocalStrings.CheckCulture, LocalStrings.ListLanguagesCulture, LocalStrings.Exists));
-            CommandManager.RegisterCustomCommands("Shell", [.. addonCommands]);
             ScreensaverManager.AddonSavers.Add("personlookup", new PersonLookupDisplay());
 
             // Then, initialize configuration in a way that no mod can play with them
@@ -170,7 +57,6 @@ namespace Nitrocid.Extras.NameGen
         public void StopAddon()
         {
             LanguageTools.RemoveCustomAction(AddonName);
-            CommandManager.UnregisterCustomCommands("Shell", [.. addonCommands.Select((ci) => ci.Command)]);
             ScreensaverManager.AddonSavers.Remove("personlookup");
             ConfigTools.UnregisterBaseSetting(nameof(NameGenSaversConfig));
         }
