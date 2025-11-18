3dd608ebb9f45a1b2effd092294e23f34a6542f4
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-11-18 17:14:42 +0300

    add - Added scaffolding for notif. icon/list widge...
    ...t
    
    ---
    
    We've added scaffolding code for the notification icon/list widgets.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Base/Users/Login/Widgets/Implementations/NotificationIcons.cs
@@ -0,0 +1,33 @@
+//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+namespace Nitrocid.Base.Users.Login.Widgets.Implementations
+{
+    internal class NotificationIcons : BaseWidget, IWidget
+    {
+        public override string Cleanup(int left, int top, int width, int height) =>
+            "";
+
+        public override string Initialize(int left, int top, int width, int height) =>
+            "";
+
+        public override string Render(int left, int top, int width, int height) =>
+            "";
+    }
+}
