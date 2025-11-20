9150884c8dfd87beb8b17d1afa1708dcd699e1a2
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-11-20 13:51:58 +0300

    add - doc - Added IsMode command
    ---
    
    We've added the IsMode command for MESH scripts.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Base/Shell/Shells/UESH/Commands/IsMode.cs
@@ -0,0 +1,75 @@
+//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Terminaux.Writer.ConsoleWriters;
+using Terminaux.Shell.Commands;
+using Terminaux.Colors.Themes.Colors;
+using Textify.Tools.Placeholder;
+using Nitrocid.Base.Kernel;
+using Nitrocid.Base.Kernel.Configuration;
+using Nitrocid.Base.Languages;
+using Nitrocid.Base.Users;
+using Nitrocid.Base.Users.Login.Motd;
+using Nitrocid.Base.Users.Windows;
+using Nitrocid.Base.Kernel.Hardware;
+
+namespace Nitrocid.Base.Shell.Shells.UESH.Commands
+{
+    /// <summary>
+    /// Returns true or false depending on the kernel mode
+    /// </summary>
+    class IsModeCommand : BaseCommand, ICommand
+    {
+
+        public override int Execute(CommandParameters parameters, ref string variableValue)
+        {
+            bool safeModeCheck = false;
+            bool debugModeCheck = false;
+            bool maintenanceModeCheck = false;
+            bool verbose = false;
+            bool result = false;
+            if (parameters.ContainsSwitch("-s"))
+                safeModeCheck = true;
+            if (parameters.ContainsSwitch("-d"))
+                debugModeCheck = true;
+            if (parameters.ContainsSwitch("-m"))
+                maintenanceModeCheck = true;
+            if (parameters.ContainsSwitch("-v"))
+                verbose = true;
+
+            if (!safeModeCheck && !debugModeCheck && !maintenanceModeCheck)
+            {
+                // TODO: NKS_SHELL_SHELLS_UESH_ISMODE_NOTSPECIFIED -> "Mode is not specified."
+                TextWriterColor.Write(LanguageTools.GetLocalized("NKS_SHELL_SHELLS_UESH_ISMODE_NOTSPECIFIED"), true, ThemeColorType.Error);
+                return 46;
+            }
+
+            if (safeModeCheck)
+                result = KernelEntry.SafeMode;
+            if (debugModeCheck)
+                result = KernelEntry.DebugMode;
+            if (maintenanceModeCheck)
+                result = KernelEntry.Maintenance;
+
+            if (verbose)
+                TextWriterColor.Write(result.ToString());
+            return 0;
+        }
+    }
+}
