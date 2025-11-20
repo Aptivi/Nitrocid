f63e951fec68e0de47edd65b89751749833bd993
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-11-20 20:22:13 +0300

    rem - Removed option to disable TNH widgets
    ---
    
    We've removed an option to disable TNH widgets in preparation for the
    overhaul of the whole design to make it more modern.
    
    ---
    
    Type: rem
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 0, 28, 32)
+                Return New Version(4, 0, 28, 33)
             End Get
         End Property
 
