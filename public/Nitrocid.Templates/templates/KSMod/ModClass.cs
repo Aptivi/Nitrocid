f068014d43f8c9d90b58af9b585f8c5d5797ab71
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-11-20 19:38:32 +0300

    add - Added scaffolding for Widget Canvas JSON
    ---
    
    We've added the foundational class for what will appear to be the Widget
    Canvas JSON.
    
    It will be used to customize both the login and the Nitrocid homepage.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModClass.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModClass.cs
@@ -9,7 +9,7 @@ namespace KSMod
 
         public string Version => "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(4, 0, 28, 31);
+        public Version MinimumSupportedApiVersion => new(4, 0, 28, 32);
 
         public ModLoadPriority LoadPriority => ModLoadPriority.Optional;
 
