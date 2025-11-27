bda5734ffa62dbaa6921ade26f7054f7e351ad91
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-11-27 14:21:47 +0300

    rem - brk|doc - Removed LanguageStudio addon
    ---
    
    The LanguageStudio addon has been finally removed as a result of the
    recent changes made to the language system, with the assistance of
    ResourceLab.
    
    ---
    
    Type: rem
    Breaking: True
    Doc Required: True
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 0, 28, 40)
+                Return New Version(4, 0, 28, 41)
             End Get
         End Property
 
