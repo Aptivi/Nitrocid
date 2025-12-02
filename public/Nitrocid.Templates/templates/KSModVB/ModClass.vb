1fb9c7ac5fd4ed44e2d498af73bbe1161551776c
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-02 13:05:29 +0300

    imp - Widget canvas renderer needs init on refresh
    ---
    
    The widget canvas renderer needs to initialize the widgets upon refresh is
    either true or the screen instance indicates that the refresh was done.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 0, 28, 43)
+                Return New Version(4, 0, 28, 44)
             End Get
         End Property
 
