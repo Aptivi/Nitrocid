f379145cc59860c51de4cbcc020e71e45a6a1511
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-05 12:28:23 +0300

    upd - Further updates to analyzer libraries
    ---
    
    Type: upd
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Locales/Actions/Cleaner.cs
+++ public/Nitrocid.Locales/Actions/Cleaner.cs
@@ -50,13 +50,13 @@ namespace Nitrocid.Locales.Actions
 
                 // Create a workspace using the instance
                 using var workspace = MSBuildWorkspace.Create();
-                workspace.WorkspaceFailed += (o, e) =>
+                workspace.RegisterWorkspaceFailedHandler((e) =>
                 {
                     if (e.Diagnostic.Kind == WorkspaceDiagnosticKind.Warning)
                         TextWriterColor.WriteColor($"Warning while loading the workspace: [{e.Diagnostic.Kind}] {e.Diagnostic.Message}", true, ConsoleColors.Yellow);
                     else
                         TextWriterColor.WriteColor($"Failed to load the workspace: [{e.Diagnostic.Kind}] {e.Diagnostic.Message}", true, ConsoleColors.Red);
-                };
+                });
 
                 // Check for Nitrocid solution
                 var solutionPath = "../../../../Nitrocid.sln";
