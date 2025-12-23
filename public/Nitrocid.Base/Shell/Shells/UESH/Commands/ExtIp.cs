4b426319faf9b38c0c5d6c36c4c51b02d16d20b4
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-23 17:33:41 +0300

    add - doc - Added extip and extip6 commands
    ---
    
    We've added commands that let you get your public IPv4 and IPv6 addresses.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: True
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Base/Shell/Shells/UESH/Commands/ExtIp.cs
@@ -0,0 +1,43 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Terminaux.Writer.ConsoleWriters;
+using Terminaux.Shell.Commands;
+using Nitrocid.Base.Network.Transfer;
+
+namespace Nitrocid.Base.Shell.Shells.UESH.Commands
+{
+    /// <summary>
+    /// Returns your public IP exposed to the internet
+    /// </summary>
+    /// <remarks>
+    /// If you want to know your public IP exposed to the internet, you can use this command to show it. Unlike private IP addresses, you'll need to connect to the internet to be able to get your external IP.
+    /// </remarks>
+    class ExtIpCommand : BaseCommand, ICommand
+    {
+
+        public override int Execute(CommandParameters parameters, ref string variableValue)
+        {
+            variableValue = NetworkTransfer.DownloadString("http://ipv4.icanhazip.com/", false).TrimEnd('\n');
+            if (!parameters.ContainsSwitch("-quiet"))
+                TextWriterColor.Write(variableValue);
+            return 0;
+        }
+    }
+}
