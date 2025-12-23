268a2a0a25d9975ccfbaa29a62def2fb64e10671
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-23 14:31:40 +0300

    add - Added the BackImplosion screensaver
    ---
    
    We've added a new screensaver!
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Screensavers/BackImplosion.cs
@@ -0,0 +1,113 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Terminaux.Writer.ConsoleWriters;
+using Nitrocid.Base.Drivers.RNG;
+using Nitrocid.Base.Kernel.Debugging;
+using Nitrocid.Base.Misc.Screensaver;
+using Nitrocid.Base.Kernel.Configuration;
+using System.Text;
+using Terminaux.Colors;
+using Terminaux.Sequences.Builder.Types;
+using Terminaux.Base;
+using Terminaux.Colors.Themes.Colors;
+using Terminaux.Writer.CyclicWriters.Graphical.Shapes;
+using Terminaux.Colors.Transformation;
+
+namespace Nitrocid.ScreensaverPacks.Screensavers
+{
+    /// <summary>
+    /// Display code for BackImplosion
+    /// </summary>
+    public class BackImplosionDisplay : BaseScreensaver, IScreensaver
+    {
+        // TODO: NKS_SCREENSAVERPACKS_BACKIMPLOSION_SETTINGS_DESC -> "Simulates the colored background fading out to black in circle starting from the middle"
+        private static int currentOuterRadius = 0;
+        private static int currentInnerRadius = 0;
+        private static Color colorStorage = Color.Empty;
+
+        /// <inheritdoc/>
+        public override string ScreensaverName =>
+            "BackImplosion";
+
+        /// <inheritdoc/>
+        public override void ScreensaverPreparation()
+        {
+            // Reset values
+            currentOuterRadius = currentInnerRadius = 0;
+
+            // Select colors
+            ResetColor();
+            base.ScreensaverPreparation();
+            ColorTools.LoadBackDry(colorStorage);
+        }
+
+        /// <inheritdoc/>
+        public override void ScreensaverLogic()
+        {
+            ConsoleWrapper.CursorVisible = false;
+            var backimplosionBuffer = new StringBuilder();
+            
+            // Draw the arc based on start and end angles that were changed
+            var arc = new Arc(ConsoleWrapper.WindowHeight, 0, 0, shapeColor: ThemeColorsTools.GetColor(ThemeColorType.Background))
+            {
+                AngleStart = 0,
+                AngleEnd = 0,
+                OuterRadius = currentOuterRadius,
+                Width = ConsoleWrapper.WindowWidth / 2,
+                Height = ConsoleWrapper.WindowHeight,
+            };
+
+            // Render the arc
+            backimplosionBuffer.Append(arc.Render());
+            TextWriterRaw.WriteRaw(backimplosionBuffer.ToString());
+
+            // Change the radius
+            currentOuterRadius++;
+            if (currentOuterRadius == ConsoleWrapper.WindowHeight + 5)
+            {
+                currentOuterRadius = 0;
+                ResetColor();
+                ColorTools.LoadBackDry(colorStorage);
+            }
+
+            // Reset resize sync
+            ConsoleResizeHandler.WasResized();
+            ScreensaverManager.Delay(ScreensaverPackInit.SaversConfig.BackImplosionDelay);
+        }
+
+        private static void ResetColor()
+        {
+            if (ScreensaverPackInit.SaversConfig.BackImplosionTrueColor)
+            {
+                int RedColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.BackImplosionMinimumRedColorLevel, ScreensaverPackInit.SaversConfig.BackImplosionMaximumRedColorLevel);
+                int GreenColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.BackImplosionMinimumGreenColorLevel, ScreensaverPackInit.SaversConfig.BackImplosionMaximumGreenColorLevel);
+                int BlueColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.BackImplosionMinimumBlueColorLevel, ScreensaverPackInit.SaversConfig.BackImplosionMaximumBlueColorLevel);
+                DebugWriter.WriteDebugConditional(Config.MainConfig.ScreensaverDebug, DebugLevel.I, "Got color (R;G;B: {0};{1};{2})", vars: [RedColorNum, GreenColorNum, BlueColorNum]);
+                colorStorage = new Color(RedColorNum, GreenColorNum, BlueColorNum);
+            }
+            else
+            {
+                int ColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.BackImplosionMinimumColorLevel, ScreensaverPackInit.SaversConfig.BackImplosionMaximumColorLevel);
+                DebugWriter.WriteDebugConditional(Config.MainConfig.ScreensaverDebug, DebugLevel.I, "Got color ({0})", vars: [ColorNum]);
+                colorStorage = new Color(ColorNum);
+            }
+        }
+    }
+}
--- public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
@@ -17710,5 +17710,195 @@ namespace Nitrocid.ScreensaverPacks.Settings
             }
         }
         #endregion
+
+        #region BackImplosion
+        private bool backImplosionTrueColor = true;
+        private int backImplosionDelay = 20;
+        private int backImplosionMinimumRedColorLevel = 0;
+        private int backImplosionMinimumGreenColorLevel = 0;
+        private int backImplosionMinimumBlueColorLevel = 0;
+        private int backImplosionMinimumColorLevel = 0;
+        private int backImplosionMaximumRedColorLevel = 255;
+        private int backImplosionMaximumGreenColorLevel = 255;
+        private int backImplosionMaximumBlueColorLevel = 255;
+        private int backImplosionMaximumColorLevel = 255;
+
+        /// <summary>
+        /// [BackImplosion] Enable truecolor support. Has a higher priority than 255 color support.
+        /// </summary>
+        public bool BackImplosionTrueColor
+        {
+            get
+            {
+                return backImplosionTrueColor;
+            }
+            set
+            {
+                backImplosionTrueColor = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] How many milliseconds to wait before making the next write?
+        /// </summary>
+        public int BackImplosionDelay
+        {
+            get
+            {
+                return backImplosionDelay;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 20;
+                backImplosionDelay = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The minimum red color level (true color)
+        /// </summary>
+        public int BackImplosionMinimumRedColorLevel
+        {
+            get
+            {
+                return backImplosionMinimumRedColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                backImplosionMinimumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The minimum green color level (true color)
+        /// </summary>
+        public int BackImplosionMinimumGreenColorLevel
+        {
+            get
+            {
+                return backImplosionMinimumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                backImplosionMinimumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The minimum blue color level (true color)
+        /// </summary>
+        public int BackImplosionMinimumBlueColorLevel
+        {
+            get
+            {
+                return backImplosionMinimumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                backImplosionMinimumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The minimum color level (255 colors or 16 colors)
+        /// </summary>
+        public int BackImplosionMinimumColorLevel
+        {
+            get
+            {
+                return backImplosionMinimumColorLevel;
+            }
+            set
+            {
+                int FinalMinimumLevel = 255;
+                if (value <= 0)
+                    value = 0;
+                if (value > FinalMinimumLevel)
+                    value = FinalMinimumLevel;
+                backImplosionMinimumColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The maximum red color level (true color)
+        /// </summary>
+        public int BackImplosionMaximumRedColorLevel
+        {
+            get
+            {
+                return backImplosionMaximumRedColorLevel;
+            }
+            set
+            {
+                if (value <= backImplosionMinimumRedColorLevel)
+                    value = backImplosionMinimumRedColorLevel;
+                if (value > 255)
+                    value = 255;
+                backImplosionMaximumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The maximum green color level (true color)
+        /// </summary>
+        public int BackImplosionMaximumGreenColorLevel
+        {
+            get
+            {
+                return backImplosionMaximumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= backImplosionMinimumGreenColorLevel)
+                    value = backImplosionMinimumGreenColorLevel;
+                if (value > 255)
+                    value = 255;
+                backImplosionMaximumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The maximum blue color level (true color)
+        /// </summary>
+        public int BackImplosionMaximumBlueColorLevel
+        {
+            get
+            {
+                return backImplosionMaximumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= backImplosionMinimumBlueColorLevel)
+                    value = backImplosionMinimumBlueColorLevel;
+                if (value > 255)
+                    value = 255;
+                backImplosionMaximumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [BackImplosion] The maximum color level (255 colors or 16 colors)
+        /// </summary>
+        public int BackImplosionMaximumColorLevel
+        {
+            get
+            {
+                return backImplosionMaximumColorLevel;
+            }
+            set
+            {
+                int FinalMaximumLevel = 255;
+                if (value <= backImplosionMinimumColorLevel)
+                    value = backImplosionMinimumColorLevel;
+                if (value > FinalMaximumLevel)
+                    value = FinalMaximumLevel;
+                backImplosionMaximumColorLevel = value;
+            }
+        }
+        #endregion
     }
 }
