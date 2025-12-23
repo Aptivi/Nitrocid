bb821275d531f5501a8e4d431bf818d94c0f751c
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-23 12:29:17 +0300

    add - Added the Pi screensaver
    ---
    
    We've added a new screensaver!
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
@@ -17119,5 +17119,195 @@ namespace Nitrocid.ScreensaverPacks.Settings
             }
         }
         #endregion
+
+        #region Pi
+        private bool piTrueColor = true;
+        private int piDelay = 20;
+        private int piMinimumRedColorLevel = 0;
+        private int piMinimumGreenColorLevel = 0;
+        private int piMinimumBlueColorLevel = 0;
+        private int piMinimumColorLevel = 0;
+        private int piMaximumRedColorLevel = 255;
+        private int piMaximumGreenColorLevel = 255;
+        private int piMaximumBlueColorLevel = 255;
+        private int piMaximumColorLevel = 255;
+
+        /// <summary>
+        /// [Pi] Enable truecolor support. Has a higher priority than 255 color support.
+        /// </summary>
+        public bool PiTrueColor
+        {
+            get
+            {
+                return piTrueColor;
+            }
+            set
+            {
+                piTrueColor = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] How many milliseconds to wait before making the next write?
+        /// </summary>
+        public int PiDelay
+        {
+            get
+            {
+                return piDelay;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 20;
+                piDelay = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The minimum red color level (true color)
+        /// </summary>
+        public int PiMinimumRedColorLevel
+        {
+            get
+            {
+                return piMinimumRedColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                piMinimumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The minimum green color level (true color)
+        /// </summary>
+        public int PiMinimumGreenColorLevel
+        {
+            get
+            {
+                return piMinimumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                piMinimumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The minimum blue color level (true color)
+        /// </summary>
+        public int PiMinimumBlueColorLevel
+        {
+            get
+            {
+                return piMinimumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                piMinimumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The minimum color level (255 colors or 16 colors)
+        /// </summary>
+        public int PiMinimumColorLevel
+        {
+            get
+            {
+                return piMinimumColorLevel;
+            }
+            set
+            {
+                int FinalMinimumLevel = 255;
+                if (value <= 0)
+                    value = 0;
+                if (value > FinalMinimumLevel)
+                    value = FinalMinimumLevel;
+                piMinimumColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The maximum red color level (true color)
+        /// </summary>
+        public int PiMaximumRedColorLevel
+        {
+            get
+            {
+                return piMaximumRedColorLevel;
+            }
+            set
+            {
+                if (value <= piMinimumRedColorLevel)
+                    value = piMinimumRedColorLevel;
+                if (value > 255)
+                    value = 255;
+                piMaximumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The maximum green color level (true color)
+        /// </summary>
+        public int PiMaximumGreenColorLevel
+        {
+            get
+            {
+                return piMaximumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= piMinimumGreenColorLevel)
+                    value = piMinimumGreenColorLevel;
+                if (value > 255)
+                    value = 255;
+                piMaximumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The maximum blue color level (true color)
+        /// </summary>
+        public int PiMaximumBlueColorLevel
+        {
+            get
+            {
+                return piMaximumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= piMinimumBlueColorLevel)
+                    value = piMinimumBlueColorLevel;
+                if (value > 255)
+                    value = 255;
+                piMaximumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [Pi] The maximum color level (255 colors or 16 colors)
+        /// </summary>
+        public int PiMaximumColorLevel
+        {
+            get
+            {
+                return piMaximumColorLevel;
+            }
+            set
+            {
+                int FinalMaximumLevel = 255;
+                if (value <= piMinimumColorLevel)
+                    value = piMinimumColorLevel;
+                if (value > FinalMaximumLevel)
+                    value = FinalMaximumLevel;
+                piMaximumColorLevel = value;
+            }
+        }
+        #endregion
     }
 }
