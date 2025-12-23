3aad10dd9087b1d5c4af9d72b52e6a3d0bccdb52
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-23 15:26:38 +0300

    add - Added the SwivelReveal screensaver
    ---
    
    We've added a new screensaver!
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Screensavers/SwivelReveal.cs
@@ -0,0 +1,128 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terminaux.Writer.ConsoleWriters;
+using Nitrocid.Base.Drivers.RNG;
+using Nitrocid.Base.Kernel.Debugging;
+using Nitrocid.Base.Misc.Screensaver;
+using Nitrocid.Base.Kernel.Configuration;
+using Terminaux.Colors;
+using Terminaux.Base;
+using Terminaux.Colors.Data;
+using Terminaux.Colors.Themes.Colors;
+using Terminaux.Base.Structures;
+
+namespace Nitrocid.ScreensaverPacks.Screensavers
+{
+    /// <summary>
+    /// Display for SwivelReveal
+    /// </summary>
+    public class SwivelRevealDisplay : BaseScreensaver, IScreensaver
+    {
+        // TODO: NKS_SCREENSAVERPACKS_SWIVELREVEAL_SETTINGS_DESC -> Reveals a background color that is shown behind the block (as in pointing the flashlight at some item in a dark room)
+        private Color targetColor = ConsoleColors.Lime;
+        private List<int> currentPosVertical = [];
+        private List<int> currentPosHorizontal = [];
+        private Coordinate clearCoords = new(-1, -1);
+
+        /// <inheritdoc/>
+        public override string ScreensaverName =>
+            "SwivelReveal";
+
+        /// <inheritdoc/>
+        public override void ScreensaverPreparation()
+        {
+            ThemeColorsTools.LoadBackground();
+            currentPosVertical.Clear();
+            currentPosHorizontal.Clear();
+
+            // Make an initial color storage
+            int RedColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.TrailsMinimumRedColorLevel, ScreensaverPackInit.SaversConfig.TrailsMaximumRedColorLevel);
+            int GreenColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.TrailsMinimumGreenColorLevel, ScreensaverPackInit.SaversConfig.TrailsMaximumGreenColorLevel);
+            int BlueColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.TrailsMinimumBlueColorLevel, ScreensaverPackInit.SaversConfig.TrailsMaximumBlueColorLevel);
+            targetColor = new(RedColorNum, GreenColorNum, BlueColorNum);
+            DebugWriter.WriteDebugConditional(Config.MainConfig.ScreensaverDebug, DebugLevel.I, "Got color (R;G;B: {0};{1};{2})", vars: [RedColorNum, GreenColorNum, BlueColorNum]);
+        }
+
+        /// <inheritdoc/>
+        public override void ScreensaverLogic()
+        {
+            ConsoleWrapper.CursorVisible = false;
+
+            // First, prepare how many dots to render according to the console size
+            int Height = ConsoleWrapper.WindowHeight - 4;
+            int Width = ConsoleWrapper.WindowWidth - 4;
+
+            // Then, go ahead and make these bars swivel themselves.
+            double FrequencyVertical = Math.PI / ScreensaverPackInit.SaversConfig.SwivelRevealVerticalFrequencyLevel;
+            double FrequencyHorizontal = Math.PI / ScreensaverPackInit.SaversConfig.SwivelRevealHorizontalFrequencyLevel;
+
+            // Set the current vertical positions
+            double TimeSecsVertical = 0.0;
+            bool isSetVertical = false;
+            while (true)
+            {
+                TimeSecsVertical += 0.1;
+                double calculatedHeight = Height * Math.Cos(FrequencyVertical * TimeSecsVertical) / 2;
+                currentPosVertical.Add((int)calculatedHeight);
+                if ((int)calculatedHeight == Height / 2 && isSetVertical)
+                    break;
+                if (!isSetVertical && (int)calculatedHeight < Height / 2)
+                    isSetVertical = true;
+            }
+
+            // Set the current horizontal positions
+            double TimeSecsHorizontal = 0.0;
+            bool isSetHorizontal = false;
+            while (true)
+            {
+                TimeSecsHorizontal += 0.1;
+                double calculatedWidth = Width * Math.Cos(FrequencyHorizontal * TimeSecsHorizontal) / 2;
+                currentPosHorizontal.Add((int)calculatedWidth);
+                if ((int)calculatedWidth == Width / 2 && isSetHorizontal)
+                    break;
+                if (!isSetHorizontal && (int)calculatedWidth < Width / 2)
+                    isSetHorizontal = true;
+            }
+
+            // Render the bars
+            int currPosVertical = currentPosVertical.FirstOrDefault(0);
+            int currPosHorizontal = currentPosHorizontal.FirstOrDefault(0);
+            int PosVertical = currPosVertical + Math.Abs(currentPosVertical.Min()) + 2;
+            int PosHorizontal = currPosHorizontal + Math.Abs(currentPosHorizontal.Min()) + 2;
+            currentPosVertical.RemoveAt(0);
+            currentPosHorizontal.RemoveAt(0);
+            if (!ConsoleResizeHandler.WasResized(false))
+            {
+                if (clearCoords.X != -1 && clearCoords.Y != -1)
+                    TextWriterWhereColor.WriteWhereColorBack(" ", clearCoords.X, clearCoords.Y, Color.Empty, ThemeColorsTools.GetColor(ThemeColorType.Background));
+                TextWriterWhereColor.WriteWhereColorBack(" ", PosHorizontal, PosVertical, Color.Empty, targetColor);
+            }
+            clearCoords = new(PosHorizontal, PosVertical);
+
+            // Reset resize sync
+            ConsoleResizeHandler.WasResized();
+            ScreensaverManager.Delay(ScreensaverPackInit.SaversConfig.SwivelRevealDelay);
+        }
+
+    }
+}
--- public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
@@ -18280,5 +18280,214 @@ namespace Nitrocid.ScreensaverPacks.Settings
             }
         }
         #endregion
+
+        #region SwivelReveal
+        private int swivelRevealDelay = 100;
+        private double swivelRevealHorizontalFrequencyLevel = 3;
+        private double swivelRevealVerticalFrequencyLevel = 8;
+        private int swivelRevealMinimumRedColorLevel = 0;
+        private int swivelRevealMinimumGreenColorLevel = 0;
+        private int swivelRevealMinimumBlueColorLevel = 0;
+        private int swivelRevealMinimumColorLevel = 0;
+        private int swivelRevealMaximumRedColorLevel = 255;
+        private int swivelRevealMaximumGreenColorLevel = 255;
+        private int swivelRevealMaximumBlueColorLevel = 255;
+        private int swivelRevealMaximumColorLevel = 255;
+
+        /// <summary>
+        /// [SwivelReveal] How many milliseconds to wait before making the next write?
+        /// </summary>
+        public int SwivelRevealDelay
+        {
+            get
+            {
+                return swivelRevealDelay;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 100;
+                swivelRevealDelay = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The level of the horizontal frequency. This is the denominator of the Pi value (3.1415926...) in mathematics, defined by <see cref="Math.PI"/>. Use this to create beautiful wavy swivelReveals!
+        /// </summary>
+        public double SwivelRevealHorizontalFrequencyLevel
+        {
+            get
+            {
+                return swivelRevealHorizontalFrequencyLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 3;
+                swivelRevealHorizontalFrequencyLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The level of the vertical frequency. This is the denominator of the Pi value (3.1415926...) in mathematics, defined by <see cref="Math.PI"/>. Use this to create beautiful wavy swivelReveals!
+        /// </summary>
+        public double SwivelRevealVerticalFrequencyLevel
+        {
+            get
+            {
+                return swivelRevealVerticalFrequencyLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 8;
+                swivelRevealVerticalFrequencyLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The minimum red color level (true color)
+        /// </summary>
+        public int SwivelRevealMinimumRedColorLevel
+        {
+            get
+            {
+                return swivelRevealMinimumRedColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                swivelRevealMinimumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The minimum green color level (true color)
+        /// </summary>
+        public int SwivelRevealMinimumGreenColorLevel
+        {
+            get
+            {
+                return swivelRevealMinimumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                swivelRevealMinimumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The minimum blue color level (true color)
+        /// </summary>
+        public int SwivelRevealMinimumBlueColorLevel
+        {
+            get
+            {
+                return swivelRevealMinimumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                swivelRevealMinimumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The minimum color level (255 colors or 16 colors)
+        /// </summary>
+        public int SwivelRevealMinimumColorLevel
+        {
+            get
+            {
+                return swivelRevealMinimumColorLevel;
+            }
+            set
+            {
+                int FinalMinimumLevel = 255;
+                if (value <= 0)
+                    value = 0;
+                if (value > FinalMinimumLevel)
+                    value = FinalMinimumLevel;
+                swivelRevealMinimumColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The maximum red color level (true color)
+        /// </summary>
+        public int SwivelRevealMaximumRedColorLevel
+        {
+            get
+            {
+                return swivelRevealMaximumRedColorLevel;
+            }
+            set
+            {
+                if (value <= swivelRevealMaximumRedColorLevel)
+                    value = swivelRevealMaximumRedColorLevel;
+                if (value > 255)
+                    value = 255;
+                swivelRevealMaximumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The maximum green color level (true color)
+        /// </summary>
+        public int SwivelRevealMaximumGreenColorLevel
+        {
+            get
+            {
+                return swivelRevealMaximumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= swivelRevealMaximumGreenColorLevel)
+                    value = swivelRevealMaximumGreenColorLevel;
+                if (value > 255)
+                    value = 255;
+                swivelRevealMaximumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The maximum blue color level (true color)
+        /// </summary>
+        public int SwivelRevealMaximumBlueColorLevel
+        {
+            get
+            {
+                return swivelRevealMaximumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= swivelRevealMaximumBlueColorLevel)
+                    value = swivelRevealMaximumBlueColorLevel;
+                if (value > 255)
+                    value = 255;
+                swivelRevealMaximumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [SwivelReveal] The maximum color level (255 colors or 16 colors)
+        /// </summary>
+        public int SwivelRevealMaximumColorLevel
+        {
+            get
+            {
+                return swivelRevealMaximumColorLevel;
+            }
+            set
+            {
+                int FinalMaximumLevel = 255;
+                if (value <= swivelRevealMaximumColorLevel)
+                    value = swivelRevealMaximumColorLevel;
+                if (value > FinalMaximumLevel)
+                    value = FinalMaximumLevel;
+                swivelRevealMaximumColorLevel = value;
+            }
+        }
+        #endregion
     }
 }
