e2e0a60943ae0cf136f8f472fc60bc391f6f9588
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-24 13:50:33 +0300

    add - Added the LineUp screensaver
    ---
    
    We've added a new screensaver!
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Screensavers/LineUp.cs
@@ -0,0 +1,120 @@
+﻿//
+// Nitrocid KS  Copyright (C) 2018-2025  Aptivi
+//
+// This file is part of Nitrocid KS
+//
+// Nitrocid KS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Nitrocid KS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY, without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+using Nitrocid.Base.Drivers.RNG;
+using Nitrocid.Base.Kernel.Debugging;
+using Nitrocid.Base.Misc.Screensaver;
+using Terminaux.Base;
+using Terminaux.Colors;
+using Nitrocid.Base.Kernel.Configuration;
+using Terminaux.Writer.ConsoleWriters;
+
+namespace Nitrocid.ScreensaverPacks.Screensavers
+{
+    /// <summary>
+    /// Display code for LineUp
+    /// </summary>
+    public class LineUpDisplay : BaseScreensaver, IScreensaver
+    {
+        // TODO: NKS_SCREENSAVERPACKS_LINEUP_SETTINGS_DESC -> Shows two lines that come from the bottom to the top while filling the console
+        private Color line1Color = Color.Empty;
+        private Color line2Color = Color.Empty;
+        private int line1PosY = -1;
+        private int line2PosY = -1;
+
+        /// <inheritdoc/>
+        public override string ScreensaverName =>
+            "LineUp";
+
+        /// <inheritdoc/>
+        public override void ScreensaverPreparation()
+        {
+            // Variable preparations
+            line1Color = SelectColor();
+            line2Color = SelectColor();
+            line1PosY = ConsoleWrapper.WindowHeight + RandomDriver.Random(-1, 10);
+            line2PosY = ConsoleWrapper.WindowHeight + RandomDriver.Random(-1, 25);
+            DebugWriter.WriteDebug(DebugLevel.I, "Line 1 position: {0}", vars: [line1PosY]);
+            DebugWriter.WriteDebug(DebugLevel.I, "Line 2 position: {0}", vars: [line1PosY]);
+        }
+
+        /// <inheritdoc/>
+        public override void ScreensaverLogic()
+        {
+            ConsoleWrapper.CursorVisible = false;
+
+            // Draw a line
+            string lineString = !string.IsNullOrWhiteSpace(ScreensaverPackInit.SaversConfig.LineUpLineChar) ? ScreensaverPackInit.SaversConfig.LineUpLineChar : "-";
+            string line = new(lineString[0], ConsoleWrapper.WindowWidth);
+            string clearLine = new(' ', ConsoleWrapper.WindowWidth);
+            if (!ConsoleResizeHandler.WasResized(false))
+            {
+                if (line1PosY < ConsoleWrapper.WindowHeight)
+                    TextWriterWhereColor.WriteWhereColor(line, 0, line1PosY, line1Color);
+                if (line2PosY < ConsoleWrapper.WindowHeight)
+                    TextWriterWhereColor.WriteWhereColor(line, 0, line2PosY, line2Color);
+            }
+
+            // Reset resize sync
+            ConsoleResizeHandler.WasResized();
+            ScreensaverManager.Delay(ScreensaverPackInit.SaversConfig.LineUpDelay);
+            if (!ConsoleResizeHandler.WasResized(false))
+            {
+                if (line1PosY < ConsoleWrapper.WindowHeight)
+                    TextWriterWhereColor.WriteWherePlain(clearLine, 0, line1PosY);
+                if (line2PosY < ConsoleWrapper.WindowHeight)
+                    TextWriterWhereColor.WriteWherePlain(clearLine, 0, line2PosY);
+            }
+
+            // Reset the position if needed
+            line1PosY--;
+            if (line1PosY < 0)
+            {
+                line1PosY = ConsoleWrapper.WindowHeight + RandomDriver.Random(-1, 10);
+                line1Color = SelectColor();
+            }
+            line2PosY--;
+            if (line2PosY < 0)
+            {
+                line2PosY = ConsoleWrapper.WindowHeight + RandomDriver.Random(-1, 25);
+                line2Color = SelectColor();
+            }
+        }
+
+        private Color SelectColor()
+        {
+            if (ScreensaverPackInit.SaversConfig.LineUpTrueColor)
+            {
+                ColorTools.LoadBackDry(new Color(ScreensaverPackInit.SaversConfig.LineUpBackgroundColor));
+                int RedColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.LineUpMinimumRedColorLevel, ScreensaverPackInit.SaversConfig.LineUpMaximumRedColorLevel);
+                int GreenColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.LineUpMinimumGreenColorLevel, ScreensaverPackInit.SaversConfig.LineUpMaximumGreenColorLevel);
+                int BlueColorNum = RandomDriver.Random(ScreensaverPackInit.SaversConfig.LineUpMinimumBlueColorLevel, ScreensaverPackInit.SaversConfig.LineUpMaximumBlueColorLevel);
+                DebugWriter.WriteDebugConditional(Config.MainConfig.ScreensaverDebug, DebugLevel.I, "Got color (R;G;B: {0};{1};{2})", vars: [RedColorNum, GreenColorNum, BlueColorNum]);
+                return new Color(RedColorNum, GreenColorNum, BlueColorNum);
+            }
+            else
+            {
+                ColorTools.LoadBackDry(new Color(ScreensaverPackInit.SaversConfig.LineUpBackgroundColor));
+                int color = RandomDriver.Random(ScreensaverPackInit.SaversConfig.LineUpMinimumColorLevel, ScreensaverPackInit.SaversConfig.LineUpMaximumColorLevel);
+                DebugWriter.WriteDebugConditional(Config.MainConfig.ScreensaverDebug, DebugLevel.I, "Got color ({0})", vars: [color]);
+                return new Color(color);
+            }
+        }
+    }
+}
--- public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
@@ -18892,5 +18892,238 @@ namespace Nitrocid.ScreensaverPacks.Settings
             }
         }
         #endregion
+
+        #region LineUp
+        private bool lineUpTrueColor = true;
+        private int lineUpDelay = 500;
+        private string lineUpLineChar = "-";
+        private string lineUpBackgroundColor = new Color(ConsoleColors.Black).PlainSequence;
+        private int lineUpMinimumRedColorLevel = 0;
+        private int lineUpMinimumGreenColorLevel = 0;
+        private int lineUpMinimumBlueColorLevel = 0;
+        private int lineUpMinimumColorLevel = 0;
+        private int lineUpMaximumRedColorLevel = 255;
+        private int lineUpMaximumGreenColorLevel = 255;
+        private int lineUpMaximumBlueColorLevel = 255;
+        private int lineUpMaximumColorLevel = 255;
+
+        /// <summary>
+        /// [LineUp] Enable truecolor support. Has a higher priority than 255 color support.
+        /// </summary>
+        public bool LineUpTrueColor
+        {
+            get
+            {
+                return lineUpTrueColor;
+            }
+            set
+            {
+                lineUpTrueColor = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] How many milliseconds to wait before making the next write?
+        /// </summary>
+        public int LineUpDelay
+        {
+            get
+            {
+                return lineUpDelay;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 500;
+                lineUpDelay = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] Line character
+        /// </summary>
+        public string LineUpLineChar
+        {
+            get
+            {
+                return lineUpLineChar;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    value = "-";
+                lineUpLineChar = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] Screensaver background color
+        /// </summary>
+        public string LineUpBackgroundColor
+        {
+            get
+            {
+                return lineUpBackgroundColor;
+            }
+            set
+            {
+                lineUpBackgroundColor = new Color(value).PlainSequence;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The minimum red color level (true color)
+        /// </summary>
+        public int LineUpMinimumRedColorLevel
+        {
+            get
+            {
+                return lineUpMinimumRedColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                lineUpMinimumRedColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The minimum green color level (true color)
+        /// </summary>
+        public int LineUpMinimumGreenColorLevel
+        {
+            get
+            {
+                return lineUpMinimumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                lineUpMinimumGreenColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The minimum blue color level (true color)
+        /// </summary>
+        public int LineUpMinimumBlueColorLevel
+        {
+            get
+            {
+                return lineUpMinimumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                lineUpMinimumBlueColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The minimum color level (255 colors or 16 colors)
+        /// </summary>
+        public int LineUpMinimumColorLevel
+        {
+            get
+            {
+                return lineUpMinimumColorLevel;
+            }
+            set
+            {
+                int FinalMinimumLevel = 255;
+                if (value <= 0)
+                    value = 0;
+                if (value > FinalMinimumLevel)
+                    value = FinalMinimumLevel;
+                lineUpMinimumColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The maximum red color level (true color)
+        /// </summary>
+        public int LineUpMaximumRedColorLevel
+        {
+            get
+            {
+                return lineUpMaximumRedColorLevel;
+            }
+            set
+            {
+                if (value <= lineUpMinimumRedColorLevel)
+                    value = lineUpMinimumRedColorLevel;
+                if (value > 255)
+                    value = 255;
+                lineUpMaximumRedColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The maximum green color level (true color)
+        /// </summary>
+        public int LineUpMaximumGreenColorLevel
+        {
+            get
+            {
+                return lineUpMaximumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= lineUpMinimumGreenColorLevel)
+                    value = lineUpMinimumGreenColorLevel;
+                if (value > 255)
+                    value = 255;
+                lineUpMaximumGreenColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The maximum blue color level (true color)
+        /// </summary>
+        public int LineUpMaximumBlueColorLevel
+        {
+            get
+            {
+                return lineUpMaximumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= lineUpMinimumBlueColorLevel)
+                    value = lineUpMinimumBlueColorLevel;
+                if (value > 255)
+                    value = 255;
+                lineUpMaximumBlueColorLevel = value;
+            }
+        }
+
+        /// <summary>
+        /// [LineUp] The maximum color level (255 colors or 16 colors)
+        /// </summary>
+        public int LineUpMaximumColorLevel
+        {
+            get
+            {
+                return lineUpMaximumColorLevel;
+            }
+            set
+            {
+                int FinalMaximumLevel = 255;
+                if (value <= lineUpMinimumColorLevel)
+                    value = lineUpMinimumColorLevel;
+                if (value > FinalMaximumLevel)
+                    value = FinalMaximumLevel;
+                lineUpMaximumColorLevel = value;
+            }
+        }
+        #endregion
     }
 }
