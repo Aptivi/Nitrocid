f202526e20be7c7db0e3efc29d5ad447aeca800b
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-24 13:13:09 +0300

    add - Added the WaveRun screensaver
    ---
    
    We've added a new screensaver!
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
+++ public/Nitrocid.Addons.Essentials/Nitrocid.ScreensaverPacks/Settings/ExtraSaversConfig.cs
@@ -18700,5 +18700,197 @@ namespace Nitrocid.ScreensaverPacks.Settings
             }
         }
         #endregion
+
+        #region WaveRun
+        private int waveRunDelay = 100;
+        private double waveRunFrequencyLevel = 3;
+        private int waveRunMinimumRedColorLevel = 0;
+        private int waveRunMinimumGreenColorLevel = 0;
+        private int waveRunMinimumBlueColorLevel = 0;
+        private int waveRunMinimumColorLevel = 0;
+        private int waveRunMaximumRedColorLevel = 255;
+        private int waveRunMaximumGreenColorLevel = 255;
+        private int waveRunMaximumBlueColorLevel = 255;
+        private int waveRunMaximumColorLevel = 255;
+
+        /// <summary>
+        /// [WaveRun] How many milliseconds to wait before making the next write?
+        /// </summary>
+        public int WaveRunDelay
+        {
+            get
+            {
+                return waveRunDelay;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 100;
+                waveRunDelay = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The level of the frequency. This is the denominator of the Pi value (3.1415926...) in mathematics, defined by <see cref="Math.PI"/>.
+        /// </summary>
+        public double WaveRunFrequencyLevel
+        {
+            get
+            {
+                return waveRunFrequencyLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 3;
+                waveRunFrequencyLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The minimum red color level (true color)
+        /// </summary>
+        public int WaveRunMinimumRedColorLevel
+        {
+            get
+            {
+                return waveRunMinimumRedColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                waveRunMinimumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The minimum green color level (true color)
+        /// </summary>
+        public int WaveRunMinimumGreenColorLevel
+        {
+            get
+            {
+                return waveRunMinimumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                waveRunMinimumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The minimum blue color level (true color)
+        /// </summary>
+        public int WaveRunMinimumBlueColorLevel
+        {
+            get
+            {
+                return waveRunMinimumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= 0)
+                    value = 0;
+                if (value > 255)
+                    value = 255;
+                waveRunMinimumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The minimum color level (255 colors or 16 colors)
+        /// </summary>
+        public int WaveRunMinimumColorLevel
+        {
+            get
+            {
+                return waveRunMinimumColorLevel;
+            }
+            set
+            {
+                int FinalMinimumLevel = 255;
+                if (value <= 0)
+                    value = 0;
+                if (value > FinalMinimumLevel)
+                    value = FinalMinimumLevel;
+                waveRunMinimumColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The maximum red color level (true color)
+        /// </summary>
+        public int WaveRunMaximumRedColorLevel
+        {
+            get
+            {
+                return waveRunMaximumRedColorLevel;
+            }
+            set
+            {
+                if (value <= waveRunMaximumRedColorLevel)
+                    value = waveRunMaximumRedColorLevel;
+                if (value > 255)
+                    value = 255;
+                waveRunMaximumRedColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The maximum green color level (true color)
+        /// </summary>
+        public int WaveRunMaximumGreenColorLevel
+        {
+            get
+            {
+                return waveRunMaximumGreenColorLevel;
+            }
+            set
+            {
+                if (value <= waveRunMaximumGreenColorLevel)
+                    value = waveRunMaximumGreenColorLevel;
+                if (value > 255)
+                    value = 255;
+                waveRunMaximumGreenColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The maximum blue color level (true color)
+        /// </summary>
+        public int WaveRunMaximumBlueColorLevel
+        {
+            get
+            {
+                return waveRunMaximumBlueColorLevel;
+            }
+            set
+            {
+                if (value <= waveRunMaximumBlueColorLevel)
+                    value = waveRunMaximumBlueColorLevel;
+                if (value > 255)
+                    value = 255;
+                waveRunMaximumBlueColorLevel = value;
+            }
+        }
+        /// <summary>
+        /// [WaveRun] The maximum color level (255 colors or 16 colors)
+        /// </summary>
+        public int WaveRunMaximumColorLevel
+        {
+            get
+            {
+                return waveRunMaximumColorLevel;
+            }
+            set
+            {
+                int FinalMaximumLevel = 255;
+                if (value <= waveRunMaximumColorLevel)
+                    value = waveRunMaximumColorLevel;
+                if (value > FinalMaximumLevel)
+                    value = FinalMaximumLevel;
+                waveRunMaximumColorLevel = value;
+            }
+        }
+        #endregion
     }
 }
