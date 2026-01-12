672a583d3bb31860b829bbc1c29446b45ae4aa74
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-01-12 11:24:44 +0300

    dcp - Aligned mod API version
    ---
    
    Type: dcp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModClass.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModClass.cs
@@ -9,7 +9,7 @@ namespace KSMod
 
         public string Version => "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(4, 0, 28, 50);
+        public Version MinimumSupportedApiVersion => new(4, 0, 28, 51);
 
         public ModLoadPriority LoadPriority => ModLoadPriority.Optional;
 
