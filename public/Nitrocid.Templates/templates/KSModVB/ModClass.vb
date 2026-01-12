6ec0245a7defaa6a50aa00b07ca91cc7668540fb
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-01-12 11:24:44 +0300

    dcp - Aligned mod API version
    ---
    
    Type: dcp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 0, 28, 50)
+                Return New Version(4, 0, 28, 51)
             End Get
         End Property
 
