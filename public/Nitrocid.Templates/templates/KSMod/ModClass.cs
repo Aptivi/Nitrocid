64ab1df9738be1ea7e7daceb02f310a778cac145
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-01-21 15:42:44 +0300

    dcp - The "Final Touches" Stage
    ---
    
    Type: dcp
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModClass.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModClass.cs
@@ -9,7 +9,7 @@ namespace KSMod
 
         public string Version => "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(4, 0, 28, 52);
+        public Version MinimumSupportedApiVersion => new(4, 0, 28, 53);
 
         public ModLoadPriority LoadPriority => ModLoadPriority.Optional;
 
--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 0, 28, 52)
+                Return New Version(4, 0, 28, 53)
             End Get
         End Property
 
