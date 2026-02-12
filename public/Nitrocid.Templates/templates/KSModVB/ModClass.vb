fa774c9c93b913ad6d99651dd1e52701e0c31fa2
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-02-12 20:46:46 +0300

    dcp - Incremented the API version
    ---
    
    We've incremented the API version due to breaking changes that happened to
    SharpCompress v0.46.0.
    
    ---
    
    Type: dcp
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 0, 28, 55)
+                Return New Version(4, 0, 28, 56)
             End Get
         End Property
 
