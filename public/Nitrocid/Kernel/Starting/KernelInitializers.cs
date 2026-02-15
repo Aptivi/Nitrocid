04f41f6bf31104a71f89b677b22ad167cc2e7519
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-02-15 14:53:27 +0300

    bkp - Fixed true color dialog not clearing screen ...
    ...on Linux
    
    ---
    
    We need to be able to clear the screen when the true color dialog exit.
    
    ---
    
    The following commits or tags are used for this backport:
      - dc368385bc3bb90ead172ff9a738b635299245e5
    
    ---
    
    Type: bkp
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid/Kernel/Starting/KernelInitializers.cs
+++ public/Nitrocid/Kernel/Starting/KernelInitializers.cs
@@ -337,7 +337,11 @@ namespace Nitrocid.Kernel.Starting
 
                 // Show first-time color calibration for first-time run
                 if (KernelEntry.FirstTime)
+                {
+                    SplashManager.BeginSplashOut(SplashContext.StartingUp);
                     ConsoleColoring.DetermineTrueColorFromUser();
+                    SplashManager.EndSplashOut(SplashContext.StartingUp);
+                }
 
                 // Check for errors
                 if (exceptions.Count > 0)
