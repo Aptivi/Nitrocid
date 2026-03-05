714fb218b9b32f7743b5a45971eedfa6bbc846b9
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-03-05 15:39:44 +0300

    pkg - Rollout of new vendor scripts for ADT
    ---
    
    Type: pkg
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

---
+++ vendor/vnd_clean.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+
+
+def vnd_clean(extra_args):
+    print("Populating directories to clean...")
+    solution = os.path.dirname(os.path.abspath(__file__ + '/../'))
+    outputs = {"bin", "api", "artifacts", "obj", "tmp",
+               "KSBuild", "KSAnalyzer", "KSTest",
+               "nitrocid-28", "nitrocid-28-lite", "docs"}
+    directories = get_dirs(solution)
+    final_directories = [d.path for d in directories
+                         if d.name in outputs]
+    for d in final_directories:
+        print("Cleaning directory %s..." % (d))
+        shutil.rmtree(d, ignore_errors=True)
+
+
+def get_dirs(directory):
+    directories = [d for d in os.scandir(directory) if d.is_dir()]
+    for d in list(directories):
+        directories.extend(get_dirs(d.path))
+    return directories
