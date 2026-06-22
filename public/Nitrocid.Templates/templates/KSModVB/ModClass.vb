d5ae58811896c6ce5125e5d438cc8410fd4d547c
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-06-22 12:46:41 +0300

    dcp - Prepare for Nitrocid v0.2.1 TP release
    ---
    
    Final touches will be made prior to the final release.
    
    ---
    
    Type: dcp
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 1, 29, 2)
+                Return New Version(4, 1, 29, 3)
             End Get
         End Property
 
