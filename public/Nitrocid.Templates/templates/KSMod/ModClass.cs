9e99b3249d0e583b03943ff49215d9fc535737cb
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-07-27 13:44:31 +0300

    rem - Removed tips addon
    ---
    
    We'll be removing the kernel tips feature in the next commit.
    
    ---
    
    Type: rem
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModClass.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModClass.cs
@@ -9,7 +9,7 @@ namespace KSMod
 
         public string Version => "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(4, 1, 29, 26);
+        public Version MinimumSupportedApiVersion => new(4, 1, 29, 27);
 
         public ModLoadPriority LoadPriority => ModLoadPriority.Optional;
 
