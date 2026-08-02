e4e0bd4c0d99b850b138f837ae4796dcfa979857
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-08-02 13:51:24 +0300

    upd - Update Terminaux
    ---
    
    Type: upd
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid.Templates/templates/KSMod/ModClass.cs
+++ public/Nitrocid.Templates/templates/KSMod/ModClass.cs
@@ -9,7 +9,7 @@ namespace KSMod
 
         public string Version => "1.0.0";
 
-        public Version MinimumSupportedApiVersion => new(4, 1, 29, 43);
+        public Version MinimumSupportedApiVersion => new(4, 1, 29, 44);
 
         public ModLoadPriority LoadPriority => ModLoadPriority.Optional;
 
--- public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
+++ public/Nitrocid.Templates/templates/KSModVB/ModClass.vb
@@ -17,7 +17,7 @@ Namespace KSModVB
 
         Public ReadOnly Property MinimumSupportedApiVersion As Version Implements IMod.MinimumSupportedApiVersion
             Get
-                Return New Version(4, 1, 29, 43)
+                Return New Version(4, 1, 29, 44)
             End Get
         End Property
 
