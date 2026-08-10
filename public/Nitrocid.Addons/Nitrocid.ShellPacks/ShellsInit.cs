8f18639f9f2f1d1f2088e7b675018b835d916cf5
Aptivi CEO <eoflaoevicecity@gmail.com>
2026-08-10 15:16:24 +0300

    ref - Refactored shell startup commands
    ---
    
    Type: ref
    Breaking: No
    Doc Required: No
    Backport Required: No
    AI Assisted: No
    Part: 1/1

--- public/Nitrocid.Addons/Nitrocid.ShellPacks/ShellsInit.cs
+++ public/Nitrocid.Addons/Nitrocid.ShellPacks/ShellsInit.cs
@@ -56,47 +56,19 @@ namespace Nitrocid.ShellPacks
     {
         private static bool gitNativeLibIsSet = false;
 
-        private readonly List<BaseCommand> archiveAddonCommands =
+        private readonly List<BaseCommand> startCommands =
         [
             new ArchiveCommand(),
-        ];
-
-        private readonly List<BaseCommand> ftpAddonCommands =
-        [
             new FtpCommandExec(),
-        ];
-
-        private readonly List<BaseCommand> gitAddonCommands =
-        [
             new GitCommandExec(),
-        ];
-
-        private readonly List<BaseCommand> httpAddonCommands =
-        [
             new HttpCommandExec(),
-        ];
-
-        private readonly List<BaseCommand> jsonAddonCommands =
-        [
             new JsonDiffCommand(),
             new JsonBeautifyCommand(),
             new JsonMinifyCommand(),
-        ];
-
-        private readonly List<BaseCommand> mailAddonCommands =
-        [
             new MailCommandExec(),
             new PopMailCommandExec(),
             new IspInfoCommand(),
-        ];
-
-        private readonly List<BaseCommand> rssAddonCommands =
-        [
             new RssCommandExec(),
-        ];
-
-        private readonly List<BaseCommand> sftpAddonCommands =
-        [
             new SftpCommandExec(),
         ];
 
@@ -123,14 +95,7 @@ namespace Nitrocid.ShellPacks
             ShellManager.RegisterShell("RSSShell", new RSSShellInfo());
             ShellManager.RegisterShell("SFTPShell", new SFTPShellInfo());
             ShellManager.RegisterShell("SqlShell", new SqlShellInfo());
-            CommandManager.RegisterCustomCommands("Shell", [.. archiveAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. ftpAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. gitAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. httpAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. jsonAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. mailAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. rssAddonCommands]);
-            CommandManager.RegisterCustomCommands("Shell", [.. sftpAddonCommands]);
+            CommandManager.RegisterCustomCommands("Shell", [.. startCommands]);
             WidgetTools.AddBaseWidget(new RssFeedSingle());
             WidgetTools.AddBaseWidget(new RssFeeds());
 
@@ -155,14 +120,7 @@ namespace Nitrocid.ShellPacks
             ShellManager.UnregisterShell("SFTPShell");
             ShellManager.UnregisterShell("SqlShell");
             ConfigTools.UnregisterBaseSetting(nameof(ShellsConfig));
-            CommandManager.UnregisterCustomCommands("Shell", [.. archiveAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. ftpAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. gitAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. httpAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. jsonAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. mailAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. rssAddonCommands.Select((ci) => ci.Command)]);
-            CommandManager.UnregisterCustomCommands("Shell", [.. sftpAddonCommands.Select((ci) => ci.Command)]);
+            CommandManager.UnregisterCustomCommands("Shell", [.. startCommands.Select((ci) => ci.Command)]);
             WidgetTools.RemoveBaseWidget(nameof(RssFeedSingle));
             WidgetTools.RemoveBaseWidget(nameof(RssFeeds));
         }
